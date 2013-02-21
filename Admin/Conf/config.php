9ce157c95a52f5a3cf4ab528a8faf1e0fbeb63d7
fanzhanao <fanzhanao@gmail.com>
2013-02-21 23:44:40 +0800

    Change-Id: I37e2f9570044ac7a27bdb44c535cc2327f3a51c2

--- Admin/Conf/config.php
+++ Admin/Conf/config.php
@@ -44,7 +44,7 @@ $config	= array(
 	'URL_ROUTER_ON'   => true, //开启路由
 	
 	'URL_ROUTE_RULES'=>array(
-				'/\/Admin\/(index\.php)?\#(\w+)$' => '/Admin/?class=1111'
+				'/\#(\w+)$' => '?class=1111'
 	),
 
     'DB_LIKE_FIELDS'=>'title|remark',
